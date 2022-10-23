a6e4ab13b2453decb047985534c5b990de97ccbc
Riley Bauer <wryr1ley@gmail.com>
2022-10-23 19:48:21 -0400

    slider

--- src/components/Footer/MainFooter.vue
+++ src/components/Footer/MainFooter.vue
@@ -1,10 +1,15 @@
 <template>
   <footer>
+    <DarkModeSwitch />
     <h6 class="footer-text">
       Copyright {{ new Date().getFullYear() }}: Riley Bauer 🐰
     </h6>
   </footer>
 </template>
+
+<script setup lang="ts">
+import DarkModeSwitch from "./DarkModeSwitch.vue";
+</script>
 
 <style lang="css" scoped>
 footer {
@@ -14,9 +19,8 @@ footer {
   bottom: 0;
   display: flex;
   align-items: center;
-  justify-content: right;
+  justify-content: space-between;
   background-color: var(--nav-bar-color);
-  box-shadow: 0px -3px 10px lightgrey;
 }
 
 .footer-text {
