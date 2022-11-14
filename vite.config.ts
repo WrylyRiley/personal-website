20990c82eb766b5427789a71adbbf5f6da062f3e
Riley Bauer <wryr1ley@gmail.com>
2022-11-13 19:43:11 -0500

    changing base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "personal-website" : "/",
+  base: "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
