8eb1c538d5c4c00479942f6acbc28b139df7c7b1
Riley Bauer <wryr1ley@gmail.com>
2022-11-13 20:15:43 -0500

    more errors

--- src/views/MainContainer.vue
+++ src/views/MainContainer.vue
@@ -1,27 +1,20 @@
 <template>
   <main class="main-container">
-    <section
-      id="title-page"
-            class="child"
-    >
+    <section id="title-page" class="child">
       <MainPageTitle class="main-title" />
     </section>
-    <section
-      id="about-me"
-      class="child"
-    >
+    <section id="about-me" class="child">
       <AboutMe />
     </section>
-    <section
-      id="my-meetup"
-      class="child"
-    >
+    <section id="my-meetup" class="child">
       <MyMeetup />
     </section>
+    <section id="resume" class="child">
       <WorkDone />
     </section>
     <!-- <section
       id="skoolie-adventure"
+      @mouseover="stash.activeLink = 'skoolie-adventure'"
       class="child"
     >
       <SkoolieAdventure />
@@ -34,8 +27,7 @@ import MainPageTitle from "@/views/TitlePage.vue";
 import AboutMe from "@/views/AboutMe.vue";
 import MyMeetup from "./MyMeetup.vue";
 import WorkDone from "./ResumePage.vue";
-import SkoolieAdventure from "./SkoolieAdventure.vue";
-import { stash } from "@/stash";
+// import SkoolieAdventure from "./SkoolieAdventure.vue";
 </script>
 
 <style lang="css" scoped>
